fa5c57a1c24a1992a98fd8b2caa9e8a513a88857
Juny0ung <joonchoi7397@gmail.com>
2026-05-09 22:25:13 +0900

    fix: https error

--- ThomasPool-api/Program.cs
+++ ThomasPool-api/Program.cs
@@ -86,7 +86,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+    app.UseHttpsRedirection();
 
 app.UseCors("AllowReactApp");
 
