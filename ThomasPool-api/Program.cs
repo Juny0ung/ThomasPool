d7f8ab07a509630e1ff1049896caffd967a6abf5
Juny0ung <joonchoi7397@gmail.com>
2026-05-09 23:05:51 +0900

    fix: render setting

--- ThomasPool-api/Program.cs
+++ ThomasPool-api/Program.cs
@@ -87,7 +87,9 @@ if (app.Environment.IsDevelopment())
 }
 
 if (!app.Environment.IsDevelopment())
-    app.UseHttpsRedirection();
+{
+    // app.UseHttpsRedirection();
+}
 
 app.UseCors("AllowReactApp");
 
